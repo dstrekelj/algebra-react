e55bcfe2b3923ede37ce6cddc80bade2218bb156
Domagoj Štrekelj <domagojstrekelj@gmail.com>
2023-03-21 19:35:25 +0100

    Update app

--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import { Navigate } from "react-router-dom";
+import { MessageOfTheDay } from "../components/MessageOfTheDay";
 import { SignInForm } from "../components/SignInForm";
 import { AppContext } from "../contexts/AppContext";
 
@@ -17,6 +18,7 @@ export function SignInPage() {
 
     return (
         <div className="sign-in-page">
+            <MessageOfTheDay />
             <div className="card">
                 <SignInForm onSubmit={handleSubmit} />
             </div>
